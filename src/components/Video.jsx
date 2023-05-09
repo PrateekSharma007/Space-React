53e44b36404f7e613aa7219815d62cf738db5157
Prateek Sharma <91019339+PrateekSharma007@users.noreply.github.com>
2023-05-10 01:14:03 +0530

    new

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -5,24 +5,25 @@ import { Link } from 'react-router-dom';
 
 
 const Video = () => {
-  return (
-    <div classsName = "hero">
-        <video autoPlay loop muted id = "video">
-        
-            <source src= {spacevideo} type = "video/mp4" />
-        
-        </video>
-        <div classname = "content">
-            <h1> Galaxy Travel</h1>
-            <p>World first civilian space travel </p>
-            <div>
-            <Link to= "/training" className='btn'> Training</Link>
-            <Link to= "/contact" className='btn btn-light'> Launch</Link>
+    return (
+        <div className="hero">
+            <video autoPlay loop muted id="video">
+
+                <source src={spacevideo} type="video/mp4" />
+
+            </video>
+            <div className="content">
+                <h1> Galaxy Travel</h1>
+                <p>World first civilian space travel </p>
+                <div>
+                <Link to="/training" className='btn'> Training</Link>
+                <Link to="/contact" className='btn btn-light'> Launch</Link>
+            </div>
+            </div>
+            
+
         </div>
-        </div>
-        
-    </div>
-  )
+    )
 }
 
 export default Video
